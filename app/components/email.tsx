0e96f464be2f8a66b8a33728fea6a8647d731ed5
nurdanakh <nurdana.khalelkyzy@fircaspian.com>
2026-05-20 10:37:07 +0500

    feat(style): implemented basic global style and components (#7)
    * feat(style): implemented basic global style and components
    
    * Testing direct push to main
    
    * test(components): add test components (#2)
    
    * Revert "test(components): add test components (#2)" (#4)
    
    This reverts commit 87defdcbf9c4663d46f8d7e68f7bf00e7f4f9978.
    
    * git oush chore: scaffold project structure and placeholder pages
    
    * feat(builder-shell): add header and progress sidebar for personal flow (#6)
    
    Needs further examination on layout
    
    ---------
    
    Co-authored-by: alisher117 <ajax_livelong@mail.ru>
    Co-authored-by: Azamat <Azamat.Omirtay@fircaspian.com>
    Co-authored-by: Alisher Maksutov <111277401+alisher117@users.noreply.github.com>

---
+++ app/components/email.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function Email(){
+    return(
+        <>
+            <label>Email
+                <input type="email" name="email" required></input>
+            </label>
+        </>
+    )
+}
